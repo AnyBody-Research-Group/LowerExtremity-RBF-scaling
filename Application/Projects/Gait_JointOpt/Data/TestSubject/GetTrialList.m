a9d181fd3370efc3a9b23bd0ec61288845978309
MLund <melund@gmail.com>
2011-10-16 00:59:57 +0200

    Added first port of the Two legged application.

---
+++ Application/Projects/Gait_JointOpt/Data/TestSubject/GetTrialList.m
@@ -0,0 +1,16 @@
+function [ TrialList ] = GetTrialList( )
+
+% TrialList = {'Trial Gait 4'};
+
+TrialList = {'Trial Gait 4'...
+            ,'Trial Standing 49'};
+
+% TrialList = {'Trial_Gait_17'...
+%     ,'Trial_Stair_Ascent_31'...
+%     ,'Trial_Stair_Descent_28'...
+%     ,'Trial_Standing_49'...
+%     ,'Trial_Sitting_38'};
+
+
+end
+
