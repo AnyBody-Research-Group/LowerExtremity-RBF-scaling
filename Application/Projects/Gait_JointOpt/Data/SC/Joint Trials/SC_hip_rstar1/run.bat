58e394d0194fd45fda17eab8b0a14126987b0c89
melund <melund@gmail.com>
2011-10-28 15:50:34 +0200

    Updated kinematics to run optimization and load anatomical values from stickfigure calculations.

---
+++ Application/Projects/Gait_JointOpt/Data/SC/Joint Trials/SC_hip_rstar1/run.bat
@@ -0,0 +1,5 @@
+
+SET AppExePath="%ProgramFiles(x86)%\AnyBody Technology\AnyBody.5.0\AnyBodyCon.exe"
+IF NOT EXIST %AppExePath% SET AppExePath="%ProgramFiles%\AnyBody Technology\AnyBody.5.0\AnyBodyCon.exe"
+%AppExePath% /m ConExe.anymcr
+exit
