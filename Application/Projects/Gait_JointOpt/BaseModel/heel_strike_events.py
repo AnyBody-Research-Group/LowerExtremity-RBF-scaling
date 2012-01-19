36793ef5a91c19805340df477b156ff254afb2e9
melund <melund@gmail.com>
2012-01-19 22:01:49 +0100

    Changed function to calculate heelstrikes. It is no longer used in the trials.

--- Application/Projects/Gait_JointOpt/BaseModel/heel_strike_events.py
+++ Application/Projects/Gait_JointOpt/BaseModel/heel_strike_events.py
@@ -39,7 +39,7 @@ def findpeaks(data, minheight, axis = None):
 
 
 
-def find_heelstrike_from_marker(marker, sacrummarker, beforetime, axis = None):
+def find_heelstrike_from_marker(marker, sacrummarker, beforetime = None, axis = None):
     # find dimension of working 
     marker = np.array(marker)
     sacrummarker = np.array(sacrummarker)
@@ -48,15 +48,19 @@ def find_heelstrike_from_marker(marker, sacrummarker, beforetime, axis = None):
     # Calculate the movement of marker with respect to sacrum marker
     data =  -(marker[:,dim]-sacrummarker[:,dim])
     # Find peaks of data (Where marker start moving backward)
-    pks,locs = findpeaks(data-np.min(data),0.2,axis = axis)
+    peak_threshold = np.max(data)-0.9*(np.max(data)-np.min(data))
+    pks,locs = findpeaks(data-np.min(data),peak_threshold,axis = axis)
     # Get last peak before 'beforetime''...
-    lastpeak = locs[np.nonzero(locs<beforetime)][-1]
-    if axis is not None:
-        axis.plot(marker[:,0])        
-    return lastpeak
+    if beforetime is not None:
+        lastpeak = locs[np.nonzero(locs<beforetime)][-1]
+        if axis is not None:
+            axis.plot(marker[:,0])        
+        return lastpeak
+    else:
+        return locs
 
 
-def find_toeoff_from_marker( marker, sacrummarker, aftertime, axis = None):
+def find_toeoff_from_marker( marker, sacrummarker, aftertime = None, axis = None):
     marker = np.array(marker)
     sacrummarker = np.array(sacrummarker)
     # find dimention with largest variability (direction of walking) 
@@ -66,10 +70,13 @@ def find_toeoff_from_marker( marker, sacrummarker, aftertime, axis = None):
     # Find peaks of data (Where marker start moving forward)
     pks,locs = findpeaks(data-np.min(data),0.2,axis = axis)
     # Get first peak after 'aftertime...
-    firstpeak = locs[np.nonzero(locs>aftertime)][0]
-    if axis is not None:
-        axis.plot(marker[:,0])        
-    return firstpeak
+    if aftertime is not None:
+        firstpeak = locs[np.nonzero(locs>aftertime)][0]
+        if axis is not None:
+            axis.plot(marker[:,0])        
+        return firstpeak
+    else:
+        return locs
             
 def find_toeoff(forcedata, threshold = 10, axis = None):
     forcedata = np.array(forcedata)
@@ -79,7 +86,7 @@ def find_toeoff(forcedata, threshold = 10, axis = None):
     else:
         return len(forcedata)
     
-def find_heelstrike (forcedata, threshold = 10, axis = None):
+def find_heelstrike (forcedata, threshold = 5, axis = None):
     forcedata = np.array(forcedata)
     indices = np.nonzero(forcedata < -threshold)[0]       
     if len(indices) > 0:
@@ -87,7 +94,7 @@ def find_heelstrike (forcedata, threshold = 10, axis = None):
     else:
         return 0    
     
-def foot_contact_times(context, f1,f2,f3,RHeel,LHeel,RToe,LToe, Sacrum, folder, avratio, axis = None):
+def foot_contact_times2(context, f1,f2,f3,RHeel,LHeel,RToe,LToe, Sacrum, folder, avratio, axis = None):
     forces = np.array([f1,f2,f3])
     print forces.shape
     peaks = np.argmin(forces,1)
@@ -135,8 +142,62 @@ def foot_contact_times(context, f1,f2,f3,RHeel,LHeel,RToe,LToe, Sacrum, folder, 
         
     return re
     
+    
+def foot_contact_times(context, f1,f2,f3,RHeel,LHeel,RToe,LToe, Sacrum, folder, avratio, axis = None):
+    # Find all Heel toe off events from forceplate data
+    hs_events = np.array([find_heelstrike(f1), find_heelstrike( f2 ), find_heelstrike( f3 )]) / avratio
+    to_events = np.array([find_toeoff(f1), find_toeoff( f2 ), find_toeoff( f3 )]) / avratio
+       
+    
+    # Find time indices when on forceplates
+    t_gaitcycle = np.min(np.diff( np.sort(hs_events) ))
+    hs_fp_start = np.round(min(hs_events)-0.2*t_gaitcycle )
+    hs_fp_end = np.round(max(hs_events)+0.2*t_gaitcycle )
+    to_fp_start = np.round(min(to_events)-0.2*t_gaitcycle )
+    to_fp_end = np.round(max(to_events)+0.2*t_gaitcycle )
+    
+    timerange_hs = np.arange(hs_fp_start,hs_fp_end)
+    timerange_to = np.arange(to_fp_start,to_fp_end)
 
+    # Get all heel strike and toe off events from kinematic data
+    hs_events_kin = np.concatenate( (find_heelstrike_from_marker(RHeel,Sacrum),
+                              find_heelstrike_from_marker(LHeel,Sacrum)) )
+    to_events_kin = np.concatenate( (find_toeoff_from_marker(RToe,Sacrum), 
+                             find_toeoff_from_marker(LToe,Sacrum)) )
+                             
+    #Find heelstrike and toeoff not on force plate
+    hs_on_fp =  np.in1d(hs_events_kin, timerange_hs)
+    hs_events_kin =  hs_events_kin[ hs_on_fp == False ]
+    to_on_fp =  np.in1d(to_events_kin, timerange_to)
+    to_events_kin =  to_events_kin[ to_on_fp == False ]
 
+    # Detect which foot is stepping on the first forceplate
+    time_fp1 = ( min(hs_events)+min(to_events) ) /2
+    
+    vel_rheel = np.diff(np.array(RHeel),axis=0)
+    vel_lheel = np.diff(np.array(LHeel),axis=0)
+    
+    to_events.sort()
+    hs_events.sort()
+    
+    if np.sqrt(sum( vel_rheel[time_fp1,:]**2)) > np.sqrt(sum( vel_lheel[time_fp1,:]**2)):        
+        hs_L = hs_events[[0,2]]
+        hs_R = np.array( [hs_events[1], hs_events_kin[0]] )
+        to_L = to_events[[0,2]]
+        to_R = np.r_[to_events_kin[to_events_kin<to_events[0]], to_events[1] ]
+    else:
+        hs_R = hs_events[[0,2]]
+        hs_L = np.array( [hs_events[1], hs_events_kin[0]] )
+        to_R = to_events[[0,2]]
+        to_L = np.r_[to_events_kin[to_events_kin<to_events[0]], to_events[1] ]
+
+    hs = np.sort( np.r_[hs_L,hs_R] )
+    to =np.sort(  np.r_[to_L,to_R] )
+    
+    events = np.round( np.sort(  np.concatenate((hs,to) ) ) )
+    
+    return (int(events[0]),int(events[1]), int(events[2]), int(events[3]),
+            int(events[4]), int(events[5]), int(events[6]), int(events[7]))
 
 if __name__ == "__main__":
     import testdata
@@ -155,9 +216,9 @@ if __name__ == "__main__":
     fig = plt.figure()
     ax = fig.add_subplot(111)
 
-    ret = foot_contact_times("",f1,f2,f3,RHeel,LHeel,RToe,LToe,Sacral, os.getcwd(), 10, axis = ax)
-    print ret
-    ax.plot(np.array(ret),np.zeros((len(ret),)), 'r^' )
+    ret1  = foot_contact_times("",f1,f2,f3,RHeel,LHeel,RToe,LToe,Sacral, os.getcwd(), 10, axis = ax)
+    print ret1
+    ax.plot(np.array(ret1),np.zeros((len(ret1),)), 'r^' )
     ax.plot(np.array(RHeel)[:,0] )
     ax.plot(np.array(LHeel)[:,0] )
 #    print 'Heelstrike: ' + str(find_heelstrike("",testdata.FzForce))
