fef50f26bf86841f02be8e3b78d6e5d7e99d4e08
melund <melund@gmail.com>
2012-01-29 22:46:36 +0100

    Added path definitions to the batchprocess script

--- Application/Projects/Gait_JointOpt/batchprocess.py
+++ Application/Projects/Gait_JointOpt/batchprocess.py
@@ -9,25 +9,38 @@ from anypytools.abcutils import AnyBatchProcess
 
 import os
 
-folder = os.path.join(os.getcwd(),'Data/SC/Gait Trials')
+folder = os.path.join(os.getcwd(),'Data/SC/Gait Trials/SC_wpgait_s6')
 
 abp = AnyBatchProcess(basepath = folder,
                       searchfile='Kinematics.main.any',
-                      num_processes = 2)
+                      num_processes = 1,
+                      paths={'H5_OUTPUT':'C:/Users/melund/Documents/AnyBody/rbf_scale_legtd/Application/Projects/Gait_JointOpt/Output/kin'})
 
-macro =  ['load "Kinematics.main.any"',
-		  'operation Main.Load_JointTrialParameters', 'run',
-		  'operation Main.LoadFootJointAxis', 'run',
-		  'operation Main.AnyBodyGaitAppModel.InverseDynamicStudy.InverseDynamics', 'run'
-		  'operation Main.SaveOutputToH5File', 'run',
-             'exit']
+macro =  ['load "Kinematics.main.any"', 
+          'operation Main.Load_JointTrialParameters',
+          'run',
+          'operation Main.JntParameterOptModel.KinematicStudy.Kinematics',
+          'run',
+          'operation Main.JntParameterOptModel.SaveKinOutputToH5File',
+          'run',
+          'exit']
 
 
-#macro =  ['load "Kinematics.main.any"', 
-#          'operation Main.Load_JointTrialParameters',
-#          'run',
-#          'operation Main.JntParameterOptModel.KinematicStudy.Kinematics',
-#          'run','exit']
+ abp = AnyBatchProcess(basepath = folder,
+                       searchfile='InversDynamics.main.any',
+                       num_processes = 1,
+                       paths={'H5_OUTPUT':'C:/Users/melund/Documents/AnyBody/rbf_scale_legtd/Application/Projects/Gait_JointOpt/Output'})
+ 
+ 
+ macro =  ['load "InversDynamics.main.any"',
+ 		  'operation Main.Load_JointTrialParameters', 'run',
+ 		  'operation Main.LoadFootJointAxis', 'run',
+ 		  'operation Main.AnyBodyGaitAppModel.InverseDynamicStudy.InverseDynamics', 'run',
+ 		  'operation Main.SaveOutputToH5File', 'run',
+              'exit']
+
+
+
           
 abp.start(macro)
 
