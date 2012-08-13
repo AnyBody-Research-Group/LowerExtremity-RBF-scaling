3bdc9cae1891895b2251e89896c3aebb702dafa5
Morten Lund <melund@gmail.com>
2012-08-13 14:59:36 +0200

    Update to the model.

--- Subjects/SC/Joint Trials/SC_knee_l2/run.bat
+++ Subjects/SC/Joint Trials/SC_knee_l2/run.bat
@@ -1,5 +1,5 @@
 
-SET AppExePath="%ProgramFiles(x86)%\AnyBody Technology\AnyBody.5.1\AnyBodyCon.exe"
+SET AppExePath="%ProgramFiles(x86)%\AnyBody Technology\AnyBody.5.2\AnyBodyCon.exe"
 IF NOT EXIST %AppExePath% SET AppExePath="%ProgramFiles%\AnyBody Technology\AnyBody.5.0\AnyBodyCon.exe"
 echo %date%, %time% > outputlog.txt
 %AppExePath% /m ConExe.anymcr |"..\..\..\wtee.exe" -a outputlog.txt
